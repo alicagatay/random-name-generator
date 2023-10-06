b1d8c08212ee26a51e8bbe3005bc403da6f7329b
Ali Cagatay <aliccagatay@gmail.com>
2023-10-06 16:21:12 +0100

    Updated `index.js` so that whenever the button is pressed, a new name will be generated and displayed at the centre of the web page.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 import names from "./nameList.js";
 import surnames from "./surnameList.js";
+let nameGeneratingButton = document.getElementById("nameGenerator");
+
+nameGeneratingButton.addEventListener("click", generateName);
+let nameDisplay = document.createElement("p");
+document.body.appendChild(nameDisplay);
+
+function generateName() {
+  let randomName = names[Math.floor(Math.random() * names.length)];
+  let randomSurname = surnames[Math.floor(Math.random() * surnames.length)];
+  let fullName = randomName + " " + randomSurname;
+  nameDisplay.innerText = fullName;
+}
